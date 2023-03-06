d985340a212ab7e2accfb1e75fc796a27e3668d2
Danick Sikkema <danick.sikkema@nlr.nl>
2023-03-06 15:28:29 +0100

    update

--- CameraFeedInPip/PreviewView.swift
+++ CameraFeedInPip/PreviewView.swift
@@ -5,4 +5,32 @@
 //  Created by Danick Sikkema on 06/03/2023.
 //
 
-import Foundation
+import UIKit
+import AVKit
+
+class PreviewView: UIView {
+    
+    var previewLayer: AVCaptureVideoPreviewLayer?
+
+    public init(captureSession: AVCaptureSession) {
+        
+        super.init(frame: .zero)
+        
+        backgroundColor = .red
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        
+        DispatchQueue.main.async {
+            self.previewLayer?.frame = CGRect(x: 0, y: 0, width: 500, height: 500)
+        }
+        
+        layer.addSublayer(previewLayer!)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- CameraFeedInPip/ViewController.swift
+++ CameraFeedInPip/ViewController.swift
@@ -6,14 +6,98 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
+    
+    private var activatePipButton: UIButton = {
+       var button = UIButton()
+        button.setTitle("Activate Pip", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .blue
+        return button
+    }()
+    
+    private var previewView: PreviewView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        addButtonToView()
+        captureCamera()
+        previewView = PreviewView(captureSession: captureSession)
+        addPreviewView()
     }
+     
+    var captureSession: AVCaptureSession = AVCaptureSession()
+    var frontInput: AVCaptureInput?
 
+    private func captureCamera() {
+        if let frontDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .front).devices.first {
+            frontInput = try! AVCaptureDeviceInput(device: frontDevice)
+        }
+        
+        captureSession.beginConfiguration()
+        if let front = frontInput, captureSession.canAddInput(front) == true {
+          captureSession.addInput(front)
+        }
 
+        captureSession.commitConfiguration()
+        
+        let backgroundQueue = DispatchQueue(label: "com.app.queue", qos: .background)
+        
+        backgroundQueue.async {
+            self.captureSession.startRunning()
+        }
+    }
+    
+    @objc
+    private func startPip() {
+        let pipVideoController = AVPictureInPictureVideoCallViewController()
+        pipVideoController.view?.addSubview(self.view)
+        
+        let pipContentSource = AVPictureInPictureController.ContentSource(
+            activeVideoCallSourceView: previewView!,
+                contentViewController: pipVideoController)
+        
+        
+        present(pipVideoController, animated: false)
+//        let pipController = AVPictureInPictureController(contentSource: pipContentSource)
+        
+//        pipVideoController.preferredContentSize = view.frame.size
+//
+//        // To start PiP automatically when app goes to background
+//        pipController.canStartPictureInPictureAutomaticallyFromInline = true
+        
+//        pipController.playerLayer.player?.play()
+//
+//        print(pipController.playerLayer.isReadyForDisplay)
+//
+//        // Or you can start PiP manually
+//        pipController.startPictureInPicture()
+    }
+    
+    private func addButtonToView() {
+        activatePipButton.addTarget(self, action: #selector(startPip), for: .touchUpInside)
+        view.addSubview(activatePipButton)
+        
+        NSLayoutConstraint.activate([
+            activatePipButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            activatePipButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 80),
+        ])
+    }
+    
+    private func addPreviewView() {
+        view.addSubview(previewView!)
+        
+        NSLayoutConstraint.activate([
+            previewView!.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 10),
+            previewView!.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 10),
+            previewView!.widthAnchor.constraint(equalToConstant: 500),
+            previewView!.heightAnchor.constraint(equalToConstant: 500),
+        ])
+    }
 }
 
